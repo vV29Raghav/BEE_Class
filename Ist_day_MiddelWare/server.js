50dee5da0bcef13642e6848ec7dc63c2ad80fe25
Raghav Verma <142114433+vV29Raghav@users.noreply.github.com>
2025-02-21 12:53:27 +0530

    Adding Middleware file

---
+++ Ist_day_MiddelWare/server.js
@@ -0,0 +1,78 @@
+// Day1
+// const express=require("express");
+// const app=express();
+
+
+// //run middleware
+// app.use(midware1);
+// app.use(midware3);
+
+
+// //get request
+// app.get("/about",(req,res)=>{
+//     console.log("running...about/");
+//     res.send("about");
+
+// });
+
+// app.use(midware2);
+
+// app.get("/",(req,res)=>{
+//     console.log("running.../");
+//     res.send("home");
+
+// });
+
+
+// //middleware
+// function midware1(req,res,next){
+//     console.log("running middleware 1..");
+//     next();
+// }
+// function midware2(req,res,next){
+//     console.log("running middleware 2..");
+//     next();
+// }
+// function midware3(req,res,next){
+//     console.log("running middleware 3.. for evrey request");
+//     next();
+// }
+
+// //port 
+// app.listen(5679,(req,res)=>{
+//     console.log("Serevr Started");
+// })
+
+
+
+
+
+
+
+// Day2
+//query parameter and route parameter,params
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
